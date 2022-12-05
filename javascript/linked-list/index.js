26d37ab93efb5a8d2cc78d303dca58df923bbaad
Kenny W. Lino <kennywlino@gmail.com>
2022-12-05 13:49:47 -0800

    Adding missing index.js and README updates.

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -37,13 +37,64 @@ class LinkedList {
   toString() {
     let current = this.head;
     let string = '';
-    while (current.next) {
+    while (current) {
       let valueStr = `{ ${current.value} } -> `;
       string = string + valueStr;
       current = current.next;
     }
-    string = string + `{ ${current.value} } -> NULL`;
+    string = string + 'NULL';
     return string;
+  }
+
+  // appends value at the end of the LinkedList
+  append(value) {
+    let node = new Node(value);
+
+    if(!this.head) {
+      this.head = node;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+  }
+
+  // inserts a value before the first matching targetValue
+  insertBefore(targetValue, value) {
+    let node = new Node(value);
+
+    let current = this.head;
+    if (current.value === targetValue) {
+      node.next = current;
+      this.head = node;
+    } else {
+      while(current) {
+        if (current.next.value === targetValue) {
+          node.next = current.next;
+          current.next = node;
+          return;
+        } else {
+          current = current.next;
+        }
+      }
+    }
+  }
+
+  // inserts a value after the first matching targetValue
+  insertAfter(targetValue, value) {
+    let node = new Node(value);
+    let current = this.head;
+    while(current) {
+      if (current.value === targetValue) {
+        node.next = current.next;
+        current.next = node;
+        return;
+      } else {
+        current = current.next;
+      }
+    }
   }
 }
 
