90b65d164b3bd31750ca398a1cc5336215ab0ca5
Kenny W. Lino <kennywlino@gmail.com>
2022-12-04 18:18:57 -0800

    Defined Node and LinkedList classes with insert, includes and toString methods.

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,49 @@
 'use strict';
 
-class LinkedList {
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  // inserts value at the head of LinkedList
+  insert(value) {
+    let node = new Node(value);
+    node.next = this.head;
+    this.head = node;
+  }
+
+  // checks for value in all nodes of LinkedList
+  // returns boolean
+  includes(value) {
+    let current = this.head;
+    while (current.next) {
+      if (current.value === value) {
+        return true;
+      } else {
+        current = current.next;
+      }
+    }
+    return false;
+  }
+
+  // return a string representation of the values in the LinkedList
+  toString() {
+    let current = this.head;
+    let string = '';
+    while (current.next) {
+      let valueStr = `{ ${current.value} } -> `;
+      string = string + valueStr;
+    }
+    string = string + 'NULL';
+    return string;
+  }
 }
 
 module.exports = LinkedList;
