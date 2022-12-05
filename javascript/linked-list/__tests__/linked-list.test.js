65379d1e758b97c237225e0362bb1f32123db37c
Kenny W. Lino <kennywlino@gmail.com>
2022-12-05 13:47:58 -0800

    Added remaining tests and updated README.

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -3,6 +3,7 @@
 // Require our linked list implementation
 const LinkedList = require('../index');
 
+// linkedList should look like 0, 3, 777, 2, 1, 4, 5, 6, 7 at the end
 describe('Linked List', () => {
   let list = new LinkedList();
   test('instantiates an empty list', () => {
@@ -48,22 +49,64 @@ describe('Linked List', () => {
     expect(list.head.next.next.next.value).toEqual(4);
   });
 
-  test('inserts a new value before the first occurrence of a target value', () => {
+  test('appends multiple nodes to the end', () => {
+    list.append(5);
+    list.append(6);
+    expect(list.head.value).toEqual(3);
+    expect(list.head.next.value).toEqual(2);
+    expect(list.head.next.next.value).toEqual(1);
+    expect(list.head.next.next.next.value).toEqual(4);
+    expect(list.head.next.next.next.next.value).toEqual(5);
+    expect(list.head.next.next.next.next.next.value).toEqual(6);
+  });
+
+  test('inserts a new value before the first occurrence of a target value located in the middle', () => {
     list.insertBefore(2, 777);
     expect(list.head.value).toEqual(3);
     expect(list.head.next.value).toEqual(777);
     expect(list.head.next.next.value).toEqual(2);
     expect(list.head.next.next.next.value).toEqual(1);
     expect(list.head.next.next.next.next.value).toEqual(4);
+    expect(list.head.next.next.next.next.next.value).toEqual(5);
+    expect(list.head.next.next.next.next.next.next.value).toEqual(6);
   });
 
-  test('inserts a new value after the first occurrence of a target value', () => {
-    list.insertAfter(1, 99);
-    expect(list.head.value).toEqual(3);
-    expect(list.head.next.value).toEqual(777);
-    expect(list.head.next.next.value).toEqual(2);
-    expect(list.head.next.next.next.value).toEqual(1);
-    expect(list.head.next.next.next.next.value).toEqual(99);
+  test('inserts a new value before the first occurrence of a target value located at the head', () => {
+    list.insertBefore(3, 0);
+    expect(list.head.value).toEqual(0);
+    expect(list.head.next.value).toEqual(3);
+    expect(list.head.next.next.value).toEqual(777);
+    expect(list.head.next.next.next.value).toEqual(2);
+    expect(list.head.next.next.next.next.value).toEqual(1);
     expect(list.head.next.next.next.next.next.value).toEqual(4);
+    expect(list.head.next.next.next.next.next.next.value).toEqual(5);
+    expect(list.head.next.next.next.next.next.next.next.value).toEqual(6);
+  });
+
+  test('inserts a new value after the first occurrence of a target value located in the middle', () => {
+    list.insertAfter(1, 99);
+    expect(list.head.value).toEqual(0);
+    expect(list.head.next.value).toEqual(3);
+    expect(list.head.next.next.value).toEqual(777);
+    expect(list.head.next.next.next.value).toEqual(2);
+    expect(list.head.next.next.next.next.value).toEqual(1);
+    expect(list.head.next.next.next.next.next.value).toEqual(99);
+    expect(list.head.next.next.next.next.next.next.value).toEqual(4);
+    expect(list.head.next.next.next.next.next.next.next.value).toEqual(5);
+    expect(list.head.next.next.next.next.next.next.next.next.value).toEqual(6);
+  });
+
+  test('inserts a new value after the first occurence of a target value located at the end', () => {
+    list.insertAfter(6, 7);
+    expect(list.head.value).toEqual(0);
+    expect(list.head.next.value).toEqual(3);
+    expect(list.head.next.next.value).toEqual(777);
+    expect(list.head.next.next.next.value).toEqual(2);
+    expect(list.head.next.next.next.next.value).toEqual(1);
+    expect(list.head.next.next.next.next.next.value).toEqual(99);
+    expect(list.head.next.next.next.next.next.next.value).toEqual(4);
+    expect(list.head.next.next.next.next.next.next.next.value).toEqual(5);
+    expect(list.head.next.next.next.next.next.next.next.next.value).toEqual(6);
+    expect(list.head.next.next.next.next.next.next.next.next.next.value).toEqual(7);
   });
 });
